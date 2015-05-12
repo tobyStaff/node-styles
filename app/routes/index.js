d86387050516013dfceb1e2405e96ee0b3913c85
Toby Stafford <toby.stafford@AG00735.addison-group.co>
2015-05-12 15:15:48 +0100

    second commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
+var fs = require( 'fs' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	var regex = /(?:#\w+\s+)?\.[\w-]+(?:\s+\w+\s*\.\w+|\s+\w+)?/ig;
+
+	fs.readFile( './styles-input/base/base.css', 'utf-8', function( err, data ) {
+
+		console.log( data.match( regex ) );
+
+		res.render('index', { title: 'Styles List', data: data.match( regex ) });
+	});
+
+
+	
+
+
 });
 
 module.exports = router;
